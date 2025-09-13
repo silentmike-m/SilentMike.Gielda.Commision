58e70190cd7188ac66e17dbea9a2b90a56d964c0
Michal Cichowski <mike@silentmike.pl>
2025-09-13 23:24:54 +0200

    customer entity unit tests

--- tests/SilentMike.Gielda.Commision.Domain.UnitTests/Customers/Entities/CustomerEntityTests.cs
+++ tests/SilentMike.Gielda.Commision.Domain.UnitTests/Customers/Entities/CustomerEntityTests.cs
@@ -1,24 +1,14 @@
 ﻿namespace SilentMike.Gielda.Commision.Domain.UnitTests.Customers.Entities;
 
+using SilentMike.Gielda.Commision.Domain.Customers.Constants;
 using SilentMike.Gielda.Commision.Domain.Customers.Entities;
 using SilentMike.Gielda.Commision.Domain.Customers.Enums;
+using SilentMike.Gielda.Commision.Domain.Customers.Exceptions;
 using SilentMike.Gielda.Commision.Domain.Customers.ValueObjects;
 
 [TestClass]
 public sealed class CustomerEntityTests
 {
-    [TestMethod, DataRow(""), DataRow("   ")]
-    public void Set_LastName_Should_ThrowCustomerLastNameException_When_FirstNameIsEmpty(string lastName)
-    {
-        // Arrange
-
-        // Act
-
-        // Assert
-        true.Should()
-            .BeFalse();
-    }
-
     [TestMethod]
     public void SetFirstName_Should_ChangeFirstName()
     {
@@ -60,12 +50,44 @@ public sealed class CustomerEntityTests
     public void SetFirstName_Should_ThrowCustomerFirstNameException_When_FirstNameIsEmpty(string firstName)
     {
         // Arrange
+        const string lastName = "Doe";
+
+        var address = new Address("New York", "123 Main St", "78-113");
+        var contact = new Contact("user@domain.com", "691 555 888");
+        var document = new Document("APX34553", DocumentType.IdentityCard);
+        var id = Guid.NewGuid();
+
+        var customer = new CustomerEntity(id, address, contact, document, "old first name", lastName);
 
         // Act
+        var action = () => customer.SetFirstName(firstName);
 
         // Assert
-        true.Should()
-            .BeFalse();
+        action.Should()
+            .Throw<CustomerFirstNameException>()
+            .Where(exception => exception.Code == ErrorCodes.CUSTOMER_EMPTY_FIRST_NAME);
+    }
+
+    [TestMethod, DataRow(""), DataRow("   ")]
+    public void SetLastName_Should_ThrowCustomerLastNameException_When_LastNameIsEmpty(string lastName)
+    {
+        // Arrange
+        const string firstName = "John";
+
+        var address = new Address("New York", "123 Main St", "78-113");
+        var contact = new Contact("user@domain.com", "691 555 888");
+        var document = new Document("APX34553", DocumentType.IdentityCard);
+        var id = Guid.NewGuid();
+
+        var customer = new CustomerEntity(id, address, contact, document, firstName, "old last name");
+
+        // Act
+        var action = () => customer.SetLastName(lastName);
+
+        // Assert
+        action.Should()
+            .Throw<CustomerLastNameException>()
+            .Where(exception => exception.Code == ErrorCodes.CUSTOMER_EMPTY_LAST_NAME);
     }
 
     [TestMethod]
@@ -144,23 +166,39 @@ public sealed class CustomerEntityTests
     public void Should_ThrowCustomerFirstNameException_When_FirstNameIsEmpty(string firstName)
     {
         // Arrange
+        const string lastName = "Doe";
+
+        var address = new Address("New York", "123 Main St", "78-113");
+        var contact = new Contact("user@domain.com", "691 555 888");
+        var document = new Document("APX34553", DocumentType.IdentityCard);
+        var id = Guid.NewGuid();
 
         // Act
+        var action = () => new CustomerEntity(id, address, contact, document, firstName, lastName);
 
         // Assert
-        true.Should()
-            .BeFalse();
+        action.Should()
+            .Throw<CustomerFirstNameException>()
+            .Where(exception => exception.Code == ErrorCodes.CUSTOMER_EMPTY_FIRST_NAME);
     }
 
     [TestMethod, DataRow(""), DataRow("   ")]
     public void Should_ThrowCustomerLastNameException_When_FirstNameIsEmpty(string lastName)
     {
         // Arrange
+        const string firstName = "John";
+
+        var address = new Address("New York", "123 Main St", "78-113");
+        var contact = new Contact("user@domain.com", "691 555 888");
+        var document = new Document("APX34553", DocumentType.IdentityCard);
+        var id = Guid.NewGuid();
 
         // Act
+        var action = () => new CustomerEntity(id, address, contact, document, firstName, lastName);
 
         // Assert
-        true.Should()
-            .BeFalse();
+        action.Should()
+            .Throw<CustomerLastNameException>()
+            .Where(exception => exception.Code == ErrorCodes.CUSTOMER_EMPTY_LAST_NAME);
     }
 }
